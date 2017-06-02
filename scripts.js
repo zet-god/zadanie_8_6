2ff2b483d88b8c196a8f62a07b720fea007454ec
Zet God <usr1@Usr1s-MacBook-Pro.local>
2017-06-02 11:58:03 +0200

    dodano plik scripts.js

---
+++ scripts.js
@@ -0,0 +1,19 @@
+var a = 10;
+var b = 7.5;
+
+var value = (a * a) + (2 * a * b) - (b * b);
+
+// werszyn łan
+
+if (value > 0) {
+	console.log('wyinik dodatni');
+} else if (value < 0) {
+	console.log('wynik ujemny');
+} else {
+	console.log('wynik równy zero');
+}
+
+// version two
+
+var whatIsBigger = value > 0 ? 'wynik dodatni' : 'wynik ujemny';
+console.log(whatIsBigger);
